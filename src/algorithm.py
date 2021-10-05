20cd219161f38d73915f4eded23073e72be4dd75
Evan Mayer <evanmayer@uchicago.edu>
2021-10-05 16:46:13 -0700

    alg skeleton work

--- src/algorithm.py
+++ src/algorithm.py
@@ -14,13 +14,14 @@ class TestSurface(object):
     Contains the mirror-dependent geometry of attachment points, 
     as-constructed/measured, relative to the mirror origin.
     '''
-    def __init__(self, Point: nw, Point: ne, Point: se):
+    def __init__(self, nw: Point, ne: Point, se: Point):
         # Define the sw corner as the origin, so always (0,0).
         # Other corner points in the plane are expressed as u,v offsets
         self.sw = Point(0., 0.)
         self.nw = Point(nw.u, nw.v)
         self.ne = Point(ne.u, ne.v)
         self.se = Point(se.u, se.v)
+        self.origin = self.sw
 
 
 class Raft(object):
@@ -28,21 +29,58 @@ class Raft(object):
     Contains the payload-dependent geometry of attachment points,
     as-constructed/measured, relative to the payload origin.
     '''
-    def __init__(self, width, height, position):
+    def __init__(self, width: float, height: float, position: Point):
         self.sw = Point(-1. * width / 2., -1. * height / 2)
         self.nw = Point(-1. * width / 2.,       height / 2)
         self.ne = Point(      width / 2.,       height / 2)
         self.se = Point(-1. * width / 2., -1. * height / 2)
         # location of payload origin in mirror coordinate frame
-        self.position = 0.0 
+        self.origin = position
 
 
 class Robot(object):
     '''
-    Contains all information and functions needed to translate a commanded
-    position into a set of motor commands via cable length deltas and 
-    velocities.
+    Contains all information and functions needed to translate a sequence of 
+    commands into a set of cable length deltas and velocities.
     '''
-    def __init__(self, TestSurface: surf, Raft: raft):
+    def __init__(self, surf: TestSurface, raft: Raft, sequence: list):
         self.surf = surf
         self.raft = raft
+        self.dt = 1.
+        self.sequence_start_time = 0.
+        self.sequence_start_elapsed = 0.
+        self.move_start_time = 0.
+        self.move_time_elapsed = 0.
+        self.sequence = sequence
+
+
+    def calc_lengths(self):
+        '''
+        Calculates the final lengths of each leg given the commanded
+        position.
+        '''
+        return
+
+
+    def calc_length_rates(self):
+        '''
+        Calculates the rates of change of each leg length given the commanded
+        position and allowed time.
+        '''
+        return
+
+
+    def calc_move_progress(self):
+        '''
+        Estimates the progress through the current move, given the allowed time
+        and time elapsed since start.
+        '''
+        return
+
+
+    def calc_sequence_progress(self):
+        '''
+        Estimates the progress through the current sequence, given the allowed time
+        and time elapsed since start.
+        '''
+        return
