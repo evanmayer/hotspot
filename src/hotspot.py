69db996571936bd7c82a620efa04c55d2c3e0adf
Evan Mayer <evanmayer@uchicago.edu>
2021-10-05 16:17:44 -0700

    start writing code

---
+++ src/hotspot.py
@@ -0,0 +1,2 @@
+# This file houses the argument parsing, program setup, the state machine,
+# calling the control algorithm, handling user/telemetry I/O, and load balancing
---
+++ src/user_io.py
@@ -0,0 +1,2 @@
+# This file defines functions for ingesting user input files into a usable data
+# structure, outputting telemetry, and visualization.
