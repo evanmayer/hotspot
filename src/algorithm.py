c222ae9c3e5f71a94fc9eee89d2b8d431abbe697
Evan Mayer <evanmayer@uchicago.edu>
2021-10-06 12:21:23 -0700

    work on basic classes

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,12 +1,14 @@
 # This file houses the algorithms necessary for calculating the control
 # quantities.
 # MKS units only.
+import numpy as np
 
 
-class Point(object):
-    def __init__(self, u, v):
-        self.u = u
-        self.v = v
+def rot(theta):
+    '''2D rotation matrix in the plane'''
+    # rotations are done like pts_mtx @ rot_mtx
+    return np.array([[np.cos(theta), -np.sin(theta)],
+                     [np.sin(theta),  np.cos(theta)]])
 
 
 class TestSurface(object):
@@ -14,13 +16,17 @@ class TestSurface(object):
     Contains the mirror-dependent geometry of attachment points, 
     as-constructed/measured, relative to the mirror origin.
     '''
-    def __init__(self, nw: Point, ne: Point, se: Point):
-        # Define the sw corner as the origin, so always (0,0).
-        # Other corner points in the plane are expressed as u,v offsets
-        self.sw = Point(0., 0.)
-        self.nw = Point(nw.u, nw.v)
-        self.ne = Point(ne.u, ne.v)
-        self.se = Point(se.u, se.v)
+    def __init__(self, nw: tuple, ne: tuple, se: tuple, sw=(0.,0.)):
+        # Define the sw corner as the origin, so default (0,0).
+        for inp in [nw, ne, se, sw]:
+            assert(len(inp) == 2,
+                'This module is 2D planar only, so points should be 2-vectors.')
+        # Corner points are expressed as x,y offsets in mirror coordinate frame
+        self.corners = np.array([[sw, nw], [se, ne]])
+        self.sw = self.corners[0,0]
+        self.se = self.corners[1,0]
+        self.nw = self.corners[0,1]
+        self.ne = self.corners[1,1]
         self.origin = self.sw
 
 
@@ -29,13 +35,29 @@ class Raft(object):
     Contains the payload-dependent geometry of attachment points,
     as-constructed/measured, relative to the payload origin.
     '''
-    def __init__(self, width: float, height: float, position: Point):
-        self.sw = Point(-1. * width / 2., -1. * height / 2)
-        self.nw = Point(-1. * width / 2.,       height / 2)
-        self.ne = Point(      width / 2.,       height / 2)
-        self.se = Point(-1. * width / 2., -1. * height / 2)
-        # location of payload origin in mirror coordinate frame
-        self.origin = position
+    def __init__(self, width: float, height: float, position: tuple):
+        assert(len(position) == 2,
+                'This module is 2D planar only, so points should be 2-vectors.')
+        # locations of attachment points in raft coordinate frame
+        self.corners = np.array(
+            [[(-width / 2., -height / 2), (-width / 2.,  height / 2)],
+             [( width / 2., -height / 2), ( width / 2.,  height / 2)]]
+        )
+        # position of the raft origin in mirror coordinate frame
+        self.position = position
+
+    @property
+    def sw(self):
+        return self.corners[0,0] + self.position
+    @property
+    def se(self):
+        return self.corners[1,0] + self.position
+    @property
+    def nw(self):
+        return self.corners[0,1] + self.position
+    @property
+    def ne(self):
+        return self.corners[1,1] + self.position
 
 
 class Robot(object):
@@ -43,7 +65,7 @@ class Robot(object):
     Contains all information and functions needed to translate a sequence of 
     commands into a set of cable length deltas and velocities.
     '''
-    def __init__(self, surf: TestSurface, raft: Raft, sequence: list):
+    def __init__(self, surf: TestSurface, raft: Raft, cmd_sequence: list):
         self.surf = surf
         self.raft = raft
         self.dt = 1.
@@ -51,7 +73,7 @@ class Robot(object):
         self.sequence_start_elapsed = 0.
         self.move_start_time = 0.
         self.move_time_elapsed = 0.
-        self.sequence = sequence
+        self.cmd_sequence = cmd_sequence
 
 
     def calc_lengths(self):
