3198ed1f966f753b83ff9a92a5e90d932c4bb230
Evan Mayer <evanmayer@uchicago.edu>
2021-10-08 14:19:16 -0700

    bounds checking on position + tests for it

--- tests/test_algorithm.py
+++ tests/test_algorithm.py
@@ -1,5 +1,6 @@
 from context import algorithm as alg
 
+
 def test_TestSurface_init():
     sw = (0,0)
     nw = (0,1)
@@ -22,6 +23,34 @@ def test_TestSurface_init():
     assert all(a == b for a,b in zip(surf.origin, sw))
 
 
+def test_TestSurface_is_inbounds():
+    sw = (0,0)
+    nw = (0,1)
+    ne = (1,1)
+    se = (1,0)
+    surf = alg.TestSurface(sw=sw, se=se, nw=nw, ne=ne)
+    assert surf.is_inbounds((0.5, 0.5))
+    assert surf.is_inbounds((0.1, 0.1))
+    assert not surf.is_inbounds((0.0, 0.5))
+    assert not surf.is_inbounds((1.0, 0.5))
+    assert not surf.is_inbounds((1.5, 1.5))
+
+
 def test_Raft_init():
-    raft = alg.Raft((0,0), 2, 3)
-    print(raft, vars(raft))
+    w = 2
+    h = 3
+    pos = (0,0)
+    raft = alg.Raft(pos, w, h)
+
+    input_list = [(-w / 2., -h / 2.), (-w / 2.,  h / 2.),
+                  ( w / 2., -h / 2.), ( w / 2.,  h / 2.)]
+    # test corner assignment
+    corner_list = raft.corners.reshape(4,2)
+    accessor_list = [raft.sw, raft.nw, raft.se, raft.ne]
+    for i, coord in enumerate(input_list):
+        # underlying matrix
+        assert all(a == b for a,b in zip(coord, corner_list[i])), \
+            f'{coord} != {corner_list[i]}'
+        # public-facing accessors
+        assert all(a == b for a,b in zip(coord, accessor_list[i])), \
+            f'{coord} != {accessor_list[i]}'
