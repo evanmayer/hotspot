3c4a5092d52e87fc130f665e97d6233306fce6e8
Evan Mayer <evanmayer@uchicago.edu>
2021-11-24 15:11:47 -0700

    first cut at beam mapper config files

--- src/constants.py
+++ src/constants.py
@@ -16,7 +16,7 @@ PWM_FREQ = 2000
 # Adafruit microstepping modifier. Divides each single step by the modifier:
 # E.g. a value of 8 splits one 1.8 deg step into eight .225 deg steps.
 # Even numbers from 2-8.
-MICROSTEP_NUM = 8
+MICROSTEP_NUM = 2
 
 # Used for converting rotational changes into stepper commands
 DEG_PER_STEP = 360. / 200. / MICROSTEP_NUM
@@ -25,8 +25,6 @@ DEG_PER_RAD = 180. / np.pi
 
 # Used for converting linear distances into rotational ones
 PULLEY_RADIUS = 0.03 / 2.
-
-DEFAULT_SPEED = 0.02 # m/s linear speed
 
 # Limits the total number of commands that may be in the command queue at once.
 # Pretty much only limited by memory.
