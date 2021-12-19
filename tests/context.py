6f9523b0b543939b20c245164c4fe6b8a9f8f6ac
Evan Mayer <evanmayer@uchicago.edu>
2021-12-19 14:21:17 -0700

    fix docs and tests after dir rename

--- tests/context.py
+++ tests/context.py
@@ -1,7 +1,7 @@
 import os
 import sys
 # https://docs.python-guide.org/writing/structure/
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'hotspot')))
 
 import algorithm
 import constants
