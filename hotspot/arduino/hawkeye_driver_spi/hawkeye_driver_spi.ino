e33c1947a3ec180d12e82ed066a39930c0a918fc
Evan Mayer <evanmayer@uchicago.edu>
2022-06-30 18:34:44 -0700

    add serial listening capability to hawkeye driver

--- hotspot/arduino/hawkeye_driver_spi/hawkeye_driver_spi.ino
+++ hotspot/arduino/hawkeye_driver_spi/hawkeye_driver_spi.ino
@@ -1,28 +1,68 @@
+// This program writes a byte from serial input to an SPI output.
+// The SPI peripheral is a shift register, 74HC595.
+#include <Adafruit_DotStar.h>
 #include <SPI.h>
 
-//latchPin is connected to ST_CP of 74HC595
+
+// payload from serial
+char data;
+// ST_CP of 74HC595
 int latchPin = 2;
-//activity indicator
-int ledPin = 13;
 SPISettings settings;
+// activity indicators
+int ledPin = 13;
+Adafruit_DotStar onBoard = Adafruit_DotStar(1, INTERNAL_DS_DATA, INTERNAL_DS_CLK, DOTSTAR_BGR);
 
-void setup() {
-  //set pins to output so you can control the shift register
-  pinMode(latchPin, OUTPUT);
-  pinMode(ledPin, OUTPUT);
 
-  // hardware SPI
-  SPI.begin();
-  settings = SPISettings(1500000, MSBFIRST, SPI_MODE0);
-  SPI.beginTransaction(settings);
+void setup()
+{
+    Serial.begin(9600);
+
+    SPI.begin();
+    settings = SPISettings(1500000, MSBFIRST, SPI_MODE0);
+    SPI.beginTransaction(settings);
+
+    pinMode(latchPin, OUTPUT);
+    pinMode(ledPin, OUTPUT);
+    onBoard.begin();
+    onBoard.show();
 }
 
-void loop() {
-  digitalWrite(ledPin, LOW);
-  SPI.beginTransaction(settings);
-  digitalWrite(latchPin, LOW);
-  SPI.transfer(0);
-  digitalWrite(latchPin, HIGH);
-  SPI.endTransaction();
-  delay(150);
+
+void writeByteSPI(long data)
+{
+    SPI.beginTransaction(settings);
+    digitalWrite(latchPin, LOW);
+    SPI.transfer(data);
+    digitalWrite(latchPin, HIGH);
+    SPI.endTransaction();
+}
+
+
+void loop()
+{
+    while (Serial.available() > 0)
+    {
+        // No timeout; intentionally hangs until next byte comes in.
+        data = (char)Serial.parseInt();
+        writeByteSPI(data);
+        if (data > 0L)
+        {
+            digitalWrite(ledPin, HIGH);
+            // interpret the 3 bits of the 8-bit char as channels in RGB.
+            onBoard.setPixelColor(0, \
+                (data & 0b001) * 255,\
+                (data & 0b010) * 255,\
+                (data & 0b100) * 255\
+            );
+            onBoard.setBrightness(100);
+            onBoard.show();
+        }
+        else
+        {
+          digitalWrite(ledPin, LOW);
+          onBoard.setBrightness(50);
+          onBoard.show();
+        }
+    }
 }
